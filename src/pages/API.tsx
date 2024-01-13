65d4158df92abbf6225ce0489697598c1437bb03
Kyro3400 <jonlichter@bmchsd.org>
2024-01-13 12:52:07 -0500

    Create API.tsx

---
+++ src/pages/API.tsx
@@ -0,0 +1,33 @@
+import React from "react"
+import Layout from "@theme/Layout"
+import JSONSchemaViewer from "@theme/JSONSchemaViewer"
+
+export default function ExamplePage(): JSX.Element {
+    const [schema, setSchema] = React.useState(undefined as undefined | Error | unknown);
+
+    React.useEffect( () => {
+        fetch(
+            // TODO Your link here
+            "https://bots-api.danbot.host/",
+            {
+                headers: {
+                    'Accept': 'application/json',
+                }
+            }
+        )
+            .then((response) => response.json())
+            .then((data) => setSchema(data))
+            .catch( (err) => setSchema(err) )
+    }, [schema])
+
+    return (
+        <Layout
+            title={`My super JSON Schema`}
+            description="Description will go into a meta tag in <head />"
+        >
+            {schema === undefined && <div>Loading ...</div>}
+            {schema !== undefined && schema instanceof Error && <div>Houston we have a problem : {schema.message}</div>}
+            {schema !== undefined && !(schema instanceof Error) && <JSONSchemaViewer schema={schema} />}
+        </Layout>
+    )
+}
