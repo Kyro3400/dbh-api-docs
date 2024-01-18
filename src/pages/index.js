86c2accf9cf733ee5200e13278d67d3b2445416e
Kyro3400 <jonlichter@bmchsd.org>
2024-01-18 17:32:21 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -36,12 +36,13 @@ export default function Home() {
       description="Description will go into a meta tag in <head />">
       <meta name="twitter:card" content="summary_large_image"/>
       <meta name="theme-color" content="#00000" />
-        <script async src="https://arc.io/widget.min.js#xUGjbaEu"></script>
       <HomepageHeader />
-      //<script async src="https://arc.io/widget.min.js#xUGjbaEu"></script>
       <main>
         <HomepageFeatures />
       </main>
+      <head>
+      <script async src="https://arc.io/widget.min.js#xUGjbaEu"></script>
+      </head>
     </Layout>
   );
 }
