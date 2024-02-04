0f2d8ed56e18ccb606213a3913e9ec985174b152
Kyro3400 <jonlichter@bmchsd.org>
2024-02-04 13:49:40 -0500

    Create site.ts

---
+++ src/constants/site.ts
@@ -0,0 +1,6 @@
+export const site: Record<string, any> = {
+    'name': 'DBH API Docs',
+    'tagline': 'Docs for DBH API',
+    'url': 'https://botapi.only-fans.club',
+    'orgName': 'danbot-hosting'
+}
